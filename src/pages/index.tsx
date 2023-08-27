3dc4588297d8b2844fe3b094613322b06115128d
okkun <okuda4645@gmail.com>
2023-08-27 15:24:30 +0900

    インポートしたデータを画面表示できるようにする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,56 @@
-import {FC} from "react";
+import { FC, useState } from "react";
+import Papa from "papaparse";
 
-const Home: FC = () => {
-  return (
-    <p>テスト</p>
-  )
+interface UserData {
+  name: string;
+  email: string;
+  role: "admin" | "normal";
+  license: "basic" | "premium";
 }
 
-export default Home
+const Home: FC = () => {
+  const [data, setData] = useState<UserData[]>([]);
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      Papa.parse<UserData>(file, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (result) => {
+          setData(result.data);
+        },
+      });
+    }
+  };
+
+  return (
+    <div>
+      <h1>CSV File Upload</h1>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      {data.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>氏名</th>
+              <th>メールアドレス</th>
+              <th>権限</th>
+              <th>ライセンス</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.email}</td>
+                <td>{row.role}</td>
+                <td>{row.license}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Home;
