612248ac0649165d3e42b7333b41e0bd92599182
okkun <okuda4645@gmail.com>
2023-08-27 16:04:59 +0900

    react-hook-formにインポートしたデータを渡すようにする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,42 @@
 import { FC, useState } from "react";
 import Papa from "papaparse";
+import { useFieldArray, useForm } from "react-hook-form";
 
-interface UserData {
-  name: string;
+type User = {
+  fullName: string;
   email: string;
   role: "admin" | "normal";
   license: "basic" | "premium";
-}
+};
 
 const Home: FC = () => {
-  const [data, setData] = useState<UserData[]>([]);
+  const [data, setData] = useState<User[]>([]);
+
+  const { getValues, reset, setValue, control, register } = useForm<{
+    users: User[];
+  }>({
+    defaultValues: {
+      users: [],
+    },
+    mode: "onChange",
+  });
+
+  const { fields } = useFieldArray({
+    control: control,
+    name: "users",
+  });
+
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-      Papa.parse<UserData>(file, {
+      Papa.parse<User>(file, {
         header: true,
         skipEmptyLines: true,
         complete: (result) => {
           setData(result.data);
+          reset({
+            users: result.data,
+          });
         },
       });
     }
@@ -27,28 +46,42 @@ const Home: FC = () => {
     <div>
       <h1>CSV File Upload</h1>
       <input type="file" accept=".csv" onChange={handleFileUpload} />
-      {data.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <th>氏名</th>
-              <th>メールアドレス</th>
-              <th>権限</th>
-              <th>ライセンス</th>
+      <table>
+        <thead>
+          <tr>
+            <th>氏名</th>
+            <th>メールアドレス</th>
+            <th>権限</th>
+            <th>ライセンス</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td>{row.fullName}</td>
+              <td>{row.email}</td>
+              <td>{row.role}</td>
+              <td>{row.license}</td>
             </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr key={index}>
-                <td>{row.name}</td>
-                <td>{row.email}</td>
-                <td>{row.role}</td>
-                <td>{row.license}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+          ))}
+          {fields.map((field, index) => (
+            <tr key={field.id}>
+              <td>
+                <input {...register(`users.${index}.fullName`)} />
+              </td>
+              <td>
+                <input {...register(`users.${index}.email`)} />
+              </td>
+              <td>
+                <input {...register(`users.${index}.role`)} />
+              </td>
+              <td>
+                <input {...register(`users.${index}.license`)} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
